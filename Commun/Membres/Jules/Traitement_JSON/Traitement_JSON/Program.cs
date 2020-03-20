2131ff0ba5126aa534d1b007755e90f7d7a6a42c
JulesDziuba <tooxi16@gmail.com>
2020-03-20 12:58:06 +0100

    update
    20/03/20

--- Commun/Membres/Jules/Traitement_JSON/Traitement_JSON/Program.cs
+++ Commun/Membres/Jules/Traitement_JSON/Traitement_JSON/Program.cs
@@ -3,12 +3,11 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Traitement_JSON
 {
-    public class JsonData
+    public class JsonDatatest
     {
         public string fruit { get; set; }
         public string size { get; set; }
@@ -22,29 +21,31 @@ namespace Traitement_JSON
             string[] urlcommands = { "sensors", "gateways", "servers" };
             string[] APIver = { "v1", "v2" };
             string serverIP = "82.231.146.148";
-            string serverPort = "11300";
-            string url = "https://" + serverIP/* + ":" + serverPort*/ + "/" + APIver[0] + "/frame/" + device_ID[0] + "/";
+            string serverPort = "8440";
+            string url = "https://" + serverIP + ":" + serverPort + "/" + APIver[0] + "/frame/" + device_ID[0];
             Console.WriteLine(url);
 
             string JsonDataSample = "{\"fruit\": \"Apple\",\"size\": \"Large\",\"color\": \"Red\"}";
 
-            JsonData = DataToJSON(JsonDataSample);
+            string JsonString = JSON.DataToJSON(JsonDataSample);
+
+            Console.WriteLine(JsonString);
 
             while (true)
             {
-                break;
                 using (var httpClient = new HttpClient())//création du client Http
                 {
                     using (var request = new HttpRequestMessage(new HttpMethod("POST"), url))//creation de la requète à envoyer,
                                                                                              //envoyer = méthode "POST", url_destinataire
                     {
+                        //request.Headers.TryAddWithoutValidation("Content-Type", "application/json");
                         request.Headers.TryAddWithoutValidation("Accept", "application/json");// ? entète de la requète http ?
 
                         var base64authorization = Convert.ToBase64String(Encoding.ASCII.GetBytes("stfelix:]u^!k_bgFKvqRb4U"));//demande d'autorisation de connexion(utilisateur:mot_de_passe)
                         request.Headers.TryAddWithoutValidation("Authorization", $"Basic {base64authorization}");// ? entète de la requète http ?
 
-                        request.Content = new StringContent("{\"frame\":\"0123abcd\",\"port\":6}");//contenu JSON de la requète
-                        request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");// ?
+                        //request.Content = new StringContent("{\"frame\":\"0123abcd\",\"port\":6}");//contenu JSON de la requète
+                        //request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");// ?
 
                         var response = await httpClient.SendAsync(request);//réponse de la requète
                         Console.WriteLine(response);//affichage de la réponse
@@ -55,18 +56,19 @@ namespace Traitement_JSON
         }
     }
 
-    class JSON
+    public class JSON
     {
-        public string DataToJSON(string Data)
+        public static string DataToJSON(string Data)
         {
             string JSONData = JsonSerializer.Serialize(Data);
             return JSONData;
         }
 
-        public string JSONToData(string JSONData)
+        public static string JSONToData(Utf8JsonReader JSONData)
         {
-            string Data = JsonSerializer.Deserialize<JsonData>(JSONData, null);
-            return Data;
+            JsonSerializerOptions JSONSOptions = null;
+            //string Data = JsonSerializer.Deserialize(JSONData, default, JSONSOptions);
+            return "Data";
         }
 
     }
