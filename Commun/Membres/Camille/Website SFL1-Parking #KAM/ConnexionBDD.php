d5c6c3c65e0bc5925123f00ba3e2bbe797eb695c
camille <lahautcamille@gmail.com>
2020-03-30 12:42:50 +0200

    changement site web + BDD + capture ecran pour dossier
    camille changement

---
+++ Commun/Membres/Camille/Website SFL1-Parking #KAM/ConnexionBDD.php
@@ -0,0 +1,22 @@
+<?php
+
+/* parametres */
+
+$host = 'localhost';
+$db = 'SFL1-BDD-PARKING';
+$user = 'root';
+$pass='';
+$charset = 'utf8mb4';
+
+$dsn ="mysql:host=$host;dbname:$db;charset=$charset";
+$options = [
+
+    PDO::ATTR_ERRMODE   => PDO::ERRMODE_EXCEPTION
+    PDO::ATTR
+
+
+
+
+]
+
+
